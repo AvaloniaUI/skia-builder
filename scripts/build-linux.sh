a1f6f8a4cf84e0b7fe8495c9006e44163a54393d
Nikita Tsukanov <keks9n@gmail.com>
2025-11-07 14:07:29 +0500

    linux

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -2,12 +2,15 @@
 set -x
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd $SCRIPT_DIR
+cd "$SCRIPT_DIR"
 cd ..
 
-IMAGE=clang-builder:latest
+# Allow overriding the build container image:
+#   SKIA_BUILD_IMAGE=myrepo/custom-image:tag ./scripts/build-linux.sh x64
+IMAGE="${CLANG_BUILDER_IMAGE:-clang-cross-builder}"
+
 docker run --rm -it \
-    -u $(id -u):$(id -g) \
+    -u "$(id -u)":"$(id -g)" \
     -v "$(pwd)":"$(pwd)" \
     -w "$(pwd)" \
     "$IMAGE" \
