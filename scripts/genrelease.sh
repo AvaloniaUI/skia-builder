9fea55e9c2a274c01d86ca91b3078cec785e7cf9
Nikita Tsukanov <keks9n@gmail.com>
2025-12-03 19:57:59 +0500

    Add RELEASE_ID.txt to skia_sdk

--- scripts/genrelease.sh
+++ scripts/genrelease.sh
@@ -6,9 +6,11 @@ SKIA_COMMIT=`git submodule status skia|awk '{print $1}'|sed 's/-//'`
 echo "Build for commit https://github.com/google/skia/commit/$SKIA_COMMIT" > release.txt  
 # include GitHub Actions run number (fallback to 'local' when not set)
 RUN_NUMBER="${GITHUB_RUN_NUMBER:-local}"
-echo "RELEASE_ID=sk_${SKIA_COMMIT}_${RUN_NUMBER}" >> $GITHUB_ENV
+RELEASE_IDENTIFIER="sk_${SKIA_COMMIT}_${RUN_NUMBER}"
+echo "RELEASE_ID=${RELEASE_IDENTIFIER}" >> $GITHUB_ENV
 
 mkdir release skia_sdk
 mv artifacts/*/* skia_sdk
 
+echo -n ${RELEASE_IDENTIFIER} > skia_sdk/RELEASE_ID.txt
 tar -czf release/skia_sdk.tar.gz skia_sdk
